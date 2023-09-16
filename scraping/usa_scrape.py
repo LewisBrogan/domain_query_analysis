98f06ba7f7b56a35e224d1fc2616f2c7250cd9b0
LewisBrogan <contact@lewisbrogan.co.uk>
2023-09-16 11:58:18 +0000

    Refactored usa_scrape.py

--- scraping/usa_scrape.py
+++ scraping/usa_scrape.py
@@ -1,40 +1,67 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import csv
+import logging
 from datetime import date
 
-current_date = date.today()
-csv_name = f'{current_date}_daily_trends_usa.csv'
-
-headOption = webdriver.FirefoxOptions()
-headOption.add_argument('-headless')
-browser = webdriver.Firefox(options=headOption)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
-browser.get('https://trends.google.com/trends/trendingsearches/daily?geo=US')
+def write_to_csv(file_name, data):
+    """Write trend data to a CSV file."""
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Title', 'Bio', 'Hits'])
+        for trend in data:
+            writer.writerow(trend)
 
-feed_list_wrappers = browser.find_elements(By.CLASS_NAME,'feed-list-wrapper')
-# returns 2 feed list arrays
 
-feed_list_wrappers
+def fetch_trends():
+    """Fetch Google daily trends."""
+    trends = []
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+    browser = webdriver.Firefox(options=options)
+
+    try:
+        browser.get(
+            'https://trends.google.com/trends/trendingsearches/daily?geo=US')
+        feed_list_wrappers = browser.find_elements(
+            By.CLASS_NAME, 'feed-list-wrapper')
+
+        for feed_list_wrapper in feed_list_wrappers:
+            details = feed_list_wrapper.find_elements(By.CLASS_NAME, 'details')
+
+            for detail in details:
+                title = detail.find_element(By.CLASS_NAME, 'details-top').text
+                bio = detail.find_element(By.CLASS_NAME, 'details-bottom').text
+                hits = detail.find_element(By.XPATH, '..').find_element(
+                    By.CLASS_NAME, "search-count-title").text
+                trends.append([title, bio, hits])
+
+    except NoSuchElementException as e:
+        logging.error(f"NoSuchElementException: {str(e)}")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {str(e)}")
+    finally:
+        browser.quit()
+    print(trends)
+    return trends
 
 
-trends = []
+def main():
+    """Main function."""
+    try:
+        current_date = date.today()
+        csv_name = f'{current_date}_daily_trends_usa.csv'
+        trends_data = fetch_trends()
 
-for feed_list_wrapper in feed_list_wrappers:
-    details = feed_list_wrapper.find_elements(By.CLASS_NAME,'details')
-    for detail in details:
+        if trends_data:
+            write_to_csv(csv_name, trends_data)
+    except FileNotFoundError as e:
+        logging.error(f"FileNotFoundError: {str(e)}")
 
-        title = detail.find_element(By.CLASS_NAME, 'details-top').text
-        bio = detail.find_element(By.CLASS_NAME, 'details-bottom').text
-        hits = detail.find_element(By.XPATH,'..').find_element(By.CLASS_NAME,"search-count-title").text
-        trends.append([title, bio, hits])
-        #print('Title: ', title, '\n', 'Bio: ', bio, '\n', 'hits: ', hits)
-        #print('\n Date: ', now)
-        
-  
-        with open(csv_name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Title', 'Bio', 'Hits'])
-            for trend in trends:
-                writer.writerow(trend)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
