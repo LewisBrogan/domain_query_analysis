09a4098a0e6be09bac7167618a90cdcdd3981a87
LEWIS BROGAN <06016658@uhi.ac.uk>
2023-09-17 18:38:26 +0100

    Modified to import fetch_trends module for refactoring

--- scraping/usa_scrape.py
+++ scraping/usa_scrape.py
@@ -1,49 +1,9 @@
-import csv
 import logging
 
 from utils.write_to_csv__utils import write_to_csv
+from scraping.utils.fetch_trends__utils import fetch_trends
 
 from datetime import date
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-def fetch_trends():
-    """Fetch Google daily trends."""
-    logging.info('Browser opened...')
-    trends = []
-    options = webdriver.FirefoxOptions()
-    options.add_argument('-headless')
-    browser = webdriver.Firefox(options=options)
-
-    try:
-        logging.info('Fetching trends......')
-        browser.get(
-            'https://trends.google.com/trends/trendingsearches/daily?geo=US')
-        feed_list_wrappers = browser.find_elements(
-            By.CLASS_NAME, 'feed-list-wrapper')
-
-        # Cleaning to be done in the Database layer using dbt, e.g. bio contains random characters
-        logging.info('Data fetched...')
-        for feed_list_wrapper in feed_list_wrappers:
-            details = feed_list_wrapper.find_elements(By.CLASS_NAME, 'details')
-            for detail in details:
-                title = detail.find_element(By.CLASS_NAME, 'details-top').text
-                bio = detail.find_element(By.CLASS_NAME, 'details-bottom').text
-                hits = detail.find_element(By.XPATH, '..').find_element(
-                    By.CLASS_NAME, "search-count-title").text
-                trends.append([title, bio, hits])
-
-    except NoSuchElementException as e:
-        logging.error(f"NoSuchElementException: {str(e)}")
-    except Exception as e:
-        logging.error(f"An unexpected error occurred: {str(e)}")
-    finally:
-        browser.quit()
-    # print(trends)
-    return trends
-
 
 def main():
     """Main function."""
