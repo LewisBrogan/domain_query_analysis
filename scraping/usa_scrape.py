400c89ef83a02e06f78e72b94d09d4bb54ee4257
LEWIS BROGAN <06016658@uhi.ac.uk>
2023-09-17 18:32:53 +0100

    Modified to import write_to_csv module for refactoring

--- scraping/usa_scrape.py
+++ scraping/usa_scrape.py
@@ -1,20 +1,13 @@
 import csv
 import logging
+
+from utils.write_to_csv__utils import write_to_csv
+
 from datetime import date
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
-
-def write_to_csv(file_name, data):
-    """Write trend data to a CSV file."""
-    logging.info('Writing data to CSV file...')
-    with open(file_name, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Title', 'Bio', 'Hits'])
-        for trend in data:
-            writer.writerow(trend)
 
 def fetch_trends():
     """Fetch Google daily trends."""
