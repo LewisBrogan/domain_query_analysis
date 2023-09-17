8185f8bdb06da7f1dcf4a35691ea2b8dc574a962
LEWIS BROGAN <06016658@uhi.ac.uk>
2023-09-17 20:56:56 +0100

    Modified to import move_data_to_region_folder module, currently commented out

--- scraping/region_daily_trend_scraper.py
+++ scraping/region_daily_trend_scraper.py
@@ -2,6 +2,7 @@ import logging
 
 from utils.write_to_csv__utils import write_to_csv
 from utils.fetch_trends__utils import fetch_trends
+# from utils.move_data_to_region_folder__utils import move_file_to_region_folder
 
 from datetime import date
 
@@ -17,7 +18,11 @@ def main():
 
             if trends_data:
                 write_to_csv(csv_name, trends_data)
-            logging.info('File saved as: %s', csv_name)
+                logging.info('File saved as: %s', csv_name)
+
+               # move_file_to_region_folder(csv_name, region_code)
+
+
     except FileNotFoundError as e:
         logging.error(f"FileNotFoundError: {str(e)}")
 
