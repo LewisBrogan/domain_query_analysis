fc10066bab3a1e4c678159661918a8e0979464fb
LEWIS BROGAN <06016658@uhi.ac.uk>
2023-09-18 21:39:48 +0100

    Added move to region folder function and added to main function

--- scraping/region_daily_trend_scraper.py
+++ scraping/region_daily_trend_scraper.py
@@ -2,7 +2,7 @@ import logging
 
 from utils.write_to_csv__utils import write_to_csv
 from utils.fetch_trends__utils import fetch_trends
-# from utils.move_data_to_region_folder__utils import move_file_to_region_folder
+from utils.move_data_to_region_folder__utils import move_file_to_region_folder
 
 from datetime import date
 
@@ -20,7 +20,7 @@ def main():
                 write_to_csv(csv_name, trends_data)
                 logging.info('File saved as: %s', csv_name)
 
-               # move_file_to_region_folder(csv_name, region_code)
+                move_file_to_region_folder(csv_name, region_code)
 
 
     except FileNotFoundError as e:
--- scraping/utils/move_data_to_region_folder__utils.py
+++ scraping/utils/move_data_to_region_folder__utils.py
@@ -1,1 +1,12 @@
-# edit
+import os
+import logging
+
+def move_file_to_region_folder(csv_name, region_code):
+    """Move file to region folder."""
+    try:
+        region_folder = f'.\scraping\data\{region_code}'
+        logging.info(f'Region folder location: {region_folder}')
+        os.makedirs(region_folder, exist_ok=True)
+        os.rename(csv_name, os.path.join(region_folder, csv_name))
+    except FileNotFoundError as e:
+        logging.error(f"FileNotFoundError: {str(e)}")
