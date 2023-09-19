f793506b39927c713a623b6033b8a234718f8ded
scottnotscott <brae@outlook.com>
2023-09-19 21:37:44 +0100

    BASIC FRONTEND

---
+++ frontend/trendscrape-frontend/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+import USData from './components/USData.vue';
+import UKData from './components/UKData.vue';
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-900 flex flex-col">
+
+  
+  <div class="flex fixed top-0 w-screen justify-center items-center text-center">
+    <Navbar/>
+  </div>
+
+  <div class="flex mb-auto flex-grow justify-center mt-10">
+    <USData/>
+  </div>
+  <div class="flex mb-auto flex-grow justify-center mt-10">
+    <UKData/>
+  </div>
+
+  <div class="fixed bottom-0 left-0 w-screen ">
+    <Footer/>
+  </div>
+
+</div>
+
+</template>
+
---
+++ frontend/trendscrape-frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+
+<ul class="flex">
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">TRENDSCRAPER</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">ABOUT</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">CONTACT</a>
+    </li>
+  </ul>
+
+</template>
